ef3d4c4fac235a027785d8f2beb89d690d4d666c
zigaw <147501246+zigaw@users.noreply.github.com>
2024-10-21 21:45:04 +0200

    Projects, Contacts - Added projects instead of placeholders. Removed form for sending emails with emailJS because of private-email problems

--- src/components/Contact/Contacts.js
+++ src/components/Contact/Contacts.js
@@ -3,11 +3,12 @@ import SplineViewer from "../SplineViewer/SplineViewer";
 import './Contacts.css';
 import emailjs from 'emailjs-com';
 
-function Contacts() {
+function Contacts({contactsRef}) {
     const [email, setEmail] = useState("");
     const [subject, setSubject] = useState("");
     const [message, setMessage] = useState("");
-
+    const [notification, setNotification] = useState("");
+    {/*}
     const handleSubmit = (e) => {
         e.preventDefault();
 
@@ -17,30 +18,37 @@ function Contacts() {
             message: message
         }, 'YOUR_USER_ID')
             .then((response) => {
-                console.log('SUCCESS!', response.status, response.text);
+                alert('Email sent successfully!');
+                setEmail("");
+                setSubject("");
+                setMessage("");
             }, (err) => {
-                console.log('FAILED...', err);
+                alert('Failed to send email. Please try again.');
             });
 
-        setEmail("");
-        setSubject("");
-        setMessage("");
+
     };
+    */}
 
     return (
-        <section id={"contact"}>
+        <section id={"contact"} ref={contactsRef}>
         <div className={"contacts"}>
             <p className={"section__text__p1"}>Get In Touch</p>
             <h1 className={"title"}>Contact Me</h1>
             <div className={"business-card"}>
                 <SplineViewer/>
             </div>
+            <div className={"contact-mail"}>
+                <a href={"mailto:ziga@weingerl.net"} className={"contact-mail-title"}>ziga@weingerl.net</a>
+            </div>
+            {/*}
             <form className="contact-form" onSubmit={handleSubmit}>
                 <input className={"form-input"} type="email" placeholder="Your Email" required value={email} onChange={(e) => setEmail(e.target.value)} />
                 <input className={"form-input"} type="text" placeholder="Subject" required value={subject} onChange={(e) => setSubject(e.target.value)} />
                 <textarea className={"form-input-textarea"} placeholder="Your Message" required value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
                 <button className={"form-submit"} type="submit">Send</button>
             </form>
+            */}
         </div>
         </section>
     );
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import project1 from "../../assets/project-1.png";
+import project2 from "../../assets/project-2.png";
+import project3 from "../../assets/project-3.png";
 import arrow_light from "../../assets/arrow-down-light.png";
 import arrow_dark from "../../assets/arrow-down-dark.png";
 function Projects({theme, showImage, projectsRef, contactsRef}) {
@@ -20,7 +22,8 @@ function Projects({theme, showImage, projectsRef, contactsRef}) {
                     <div className={"article-container"}>
                         <img src={project1} alt={"Project 1"} className={"project-img"}/>
                     </div>
-                    <h2 clasName={"experience-sub-title project-title"}>Project One</h2>
+                    <h2 clasName={"experience-sub-title project-title"}>My Website</h2>
+                    <p>This responsive website, built using React.js, was developed to demonstrate my expertise and skills. The project features a modern, visually appealing design with smooth animations and an intuitive, user-friendly interface. Key sections include a portfolio, an about me page, and a contact form, all fully optimized for seamless performance across different devices and screen sizes.</p>
                     {/*
                     <Link to={"/https://github.com/"}>
                     <div className={"btn btn-color-2 project-btn"} >GitHub</div>
@@ -32,9 +35,10 @@ function Projects({theme, showImage, projectsRef, contactsRef}) {
                 </div>
                 <div className={"details-container color-container"}>
                     <div className={"article-container"}>
-                        <img src={project1} alt={"Project 1"} className={"project-img"}/>
+                        <img src={project2} alt={"Project 2"} className={"project-img"}/>
                     </div>
-                    <h2 clasName={"experience-sub-title project-title"}>Project Two</h2>
+                    <h2 clasName={"experience-sub-title project-title"}>Shopping List Website</h2>
+                    <p>During my second year at university, I collaborated with two other students to develop a website that allowed users to add groceries to a shopping list and calculate which local store offered the lowest total price. We built Python web scrapers to collect product data from various grocery stores and stored this information in a MongoDB database. The project was successfully deployed on Azure, providing users with real-time price comparisons.</p>
                     {/*
                     <Link to={"/https://github.com/"}>
                     <div className={"btn btn-color-2 project-btn"} >GitHub</div>
@@ -46,9 +50,11 @@ function Projects({theme, showImage, projectsRef, contactsRef}) {
                 </div>
                 <div className={"details-container color-container"}>
                     <div className={"article-container"}>
-                        <img src={project1} alt={"Project 1"} className={"project-img"}/>
+                        <img src={project3} alt={"Project 3"} className={"project-img"}/>
                     </div>
-                    <h2 clasName={"experience-sub-title project-title"}>Project Three</h2>
+                    <h2 clasName={"experience-sub-title project-title"}>Small School Projects</h2>
+                    <p>Over the past two years, I have worked on a variety of projects for different courses, ranging from websites to games and automation scripts. These include games developed with LibGDX, mobile apps built using React Native and Kotlin, and projects exploring how RAM works through locking and unlocking processes in an Operating Systems class. Each project helped me deepen my understanding of key technologies like web development, game design, mobile development, and system architecture.</p>
+
                     {/*
                     <Link to={"/https://github.com/"}>
                     <div className={"btn btn-color-2 project-btn"} >GitHub</div>
